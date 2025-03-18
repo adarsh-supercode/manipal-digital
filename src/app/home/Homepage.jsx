362d88ec5f7d8f5d58298677cfeb575c3b1bbaa3
adarsh-supercode <adarsh.p@supercode.in>
2025-03-18 18:44:58 +0530

    Update Homepage.jsx

--- src/app/home/Homepage.jsx
+++ src/app/home/Homepage.jsx
@@ -1,13 +1,10 @@
 
-import Banner from './components/Banner'
-import FeaturedPosts from './components/FeaturedPosts'
 import GridSection from './components/GridSection'
 
 export default function Homepage() {
 
   return (
     <div>
-      <Banner/>
       <GridSection/>
     </div>
   )
